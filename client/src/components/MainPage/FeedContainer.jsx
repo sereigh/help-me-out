3a7c3d410884ed010242979e7b2fb8a1c3c658e3
Kyle Johnson <zion4to6@gmail.com>
2021-04-05 16:54:06 -0700

    Adds basic feed display

--- client/src/components/MainPage/FeedContainer.jsx
+++ client/src/components/MainPage/FeedContainer.jsx
@@ -1,7 +1,37 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const FeedContainer = ({ data }) => (
-  <div>Feed</div>
-)
+  <div>
+    {data.map((item) => (
+      <>
+        <span>{item.project_name ? `${item.project_name}` : `${item.tool_name}`}</span>
+        <br />
+      </>
+    ))}
+  </div>
+);
+
+FeedContainer.propTypes = {
+  data: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.shape({
+      tool_name: PropTypes.string,
+      tool_photos: PropTypes.arrayOf(PropTypes.string),
+      tool_owner: PropTypes.string,
+      help: PropTypes.bool,
+    })),
+    PropTypes.arrayOf(PropTypes.shape({
+      project_name: PropTypes.string.isRequired,
+      project_description: PropTypes.string,
+      project_owner: PropTypes.string.isRequired,
+      help: PropTypes.bool.isRequired,
+      project_photos: PropTypes.arrayOf(PropTypes.string),
+    })),
+  ]),
+};
+
+FeedContainer.defaultProps = {
+  data: [{}],
+};
 
 export default FeedContainer;
