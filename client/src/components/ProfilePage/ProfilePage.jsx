778372001db70b8cd1b706ccddd1898d89f04757
Gareth Rogers <garethdavisrogers@gmail.com>
2021-04-05 18:28:16 -0500

    added route references and add,delete,put function skeletons

--- client/src/components/ProfilePage/ProfilePage.jsx
+++ client/src/components/ProfilePage/ProfilePage.jsx
@@ -3,14 +3,33 @@ import UserProjects from "./UserProjects.jsx";
 import UserTools from "./UserTools.jsx";
 import UserInfo from "./UserInfo.jsx";
 import sampleUser from "../../../../server/database/data/sampleUser.json";
+// import postProjects from "../../../../server/routes/projects/postProjects";
+// import deleteProjects from "../../../../server/routes/projects/deleteProjects";
+// import putProjects from "../../../../server/routes/projects/putProjects";
+// import postTools from "../../../../server/routes/tools/postTools";
+// import deleteTools from "../../../../server/routes/tools/postTools";
+// import putTools from "../../../../server/routes/tools/putTools";
+// import putUsers from "../../../../server/routes/users/putUser";
 
 class ProfilePage extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = { userInfo: {}, userTools: {}, userProjects: {} };
+
+    this.toggleHelp = this.toggleHelp.bind(this);
+    this.addProfileItem = this.addProfileItem.bind(this);
+    this.deleteProfileItem = this.deleteProfileItem.bind(this);
+    this.putProfileItemItem = this.putProfileItem.bind(this);
   }
 
+  toggleHelp() {}
+
+  addProfileItem() {}
+
+  deleteProfileItem() {}
+
+  putProfileItem() {}
   //testing only!
   componentDidMount() {
     const sampleUserInfo = sampleUser;
@@ -19,6 +38,7 @@ class ProfilePage extends React.Component {
         name: sampleUserInfo.name,
         zip: sampleUserInfo.zip,
         handy: sampleUserInfo.handy,
+        photo: sampleUserInfo.photo || null,
       },
       userTools: sampleUserInfo.tools,
       userProjects: sampleUserInfo.projects,
@@ -31,7 +51,13 @@ class ProfilePage extends React.Component {
       return (
         <div>
           <UserInfo userInfo={userInfo} />
-          <UserProjects userProjects={userProjects} />
+          <UserProjects
+            userProjects={userProjects}
+            toggleHelp={this.toggleHelp}
+            addProfileItem={this.addProfileItem}
+            deleteProfileItem={this.deleteProfileItem}
+            putProfileItem={this.putProfileItem}
+          />
           <UserTools userTools={userTools} />
         </div>
       );
--- client/src/components/ProfilePage/UserInfo.jsx
+++ client/src/components/ProfilePage/UserInfo.jsx
@@ -2,7 +2,8 @@ import React from "react";
 
 const UserInfo = (props) => {
   return (
-    <div>
+    <div className="user-info">
+      <img src={props.userInfo.photo} />
       <div>{props.userInfo.name}</div>
       <div>{props.userInfo.zip}</div>
       <div>{props.userInfo.handy}</div>
