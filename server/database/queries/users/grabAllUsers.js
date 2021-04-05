71612c0cb38b3cb7de94af37b2dc256fdd28f97d
Krista <kb9286@gmail.com>
2021-04-05 12:46:54 -0500

    adding sort by updatedAt to getAllUsers

--- server/database/queries/users/grabAllUsers.js
+++ server/database/queries/users/grabAllUsers.js
@@ -4,6 +4,7 @@ exports.grabAllUsers = (cb) => {
   db.User.find({}).
   populate('tools').
   populate('projects').
+  sort({updatedAt: -1}).
   exec((err, result) => {
     if (err) { cb(err, null); } else { cb(null, result); }
   })
