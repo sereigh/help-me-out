194e1e841c1793d9af17c56655471a08e1621736
Kyle Johnson <zion4to6@gmail.com>
2021-04-05 16:35:44 -0700

    Adds user prop validation

--- client/src/components/MainPage/MainPage.jsx
+++ client/src/components/MainPage/MainPage.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import sampleData from '../../../../server/database/data/sampleFeed.json';
 
@@ -79,5 +80,30 @@ class MainPage extends React.Component {
     );
   }
 }
+
+MainPage.propTypes = {
+  user: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    zip: PropTypes.number.isRequired,
+    password: PropTypes.string.isRequired,
+    photo: PropTypes.string,
+    handy: PropTypes.number.isRequired,
+    report: PropTypes.number.isRequired,
+    tools: PropTypes.arrayOf(PropTypes.shape({
+      tool_name: PropTypes.string,
+      tool_photos: PropTypes.arrayOf(PropTypes.string),
+      tool_owner: PropTypes.string,
+      help: PropTypes.bool,
+    })),
+    projects: PropTypes.arrayOf(PropTypes.shape({
+      project_name: PropTypes.string.isRequired,
+      project_description: PropTypes.string,
+      project_owner: PropTypes.string.isRequired,
+      help: PropTypes.bool.isRequired,
+      project_photos: PropTypes.arrayOf(PropTypes.string),
+    })),
+    favorites: PropTypes.arrayOf(PropTypes.string),
+  }).isRequired,
+};
 
 export default MainPage;
