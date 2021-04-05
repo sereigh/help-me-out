15c427f68e22af36d287e1e0474fd997d4139d2d
Krista <kb9286@gmail.com>
2021-04-05 10:30:24 -0500

    adding populate to get query

--- server/database/queries/users/grabUser.js
+++ server/database/queries/users/grabUser.js
@@ -1,7 +1,13 @@
 const db = require('../../index.js');
 
 exports.grabUser = (user, cb) => {
-  db.User.findById({ _id: user }, (err, result) => {
+  db.User.findById({_id: user}).
+  populate('tools').
+  populate('projects').
+  exec((err, result) => {
     if (err) { cb(err, null); } else { cb(null, result); }
-  });
+  })
+  // db.User.findByObjectId({ _id: user }, (err, result) => {
+  //   if (err) { cb(err, null); } else { cb(null, result); }
+  // });
 };
