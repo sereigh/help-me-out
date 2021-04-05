bf70a0e6bc9ae81efc517db3e338082acbc25e76
Kyle Johnson <zion4to6@gmail.com>
2021-04-05 16:05:16 -0700

    Adds filtering logic

--- client/src/components/MainPage/MainPage.jsx
+++ client/src/components/MainPage/MainPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import data from '../../../../server/database/data/sampleFeed.json';
+import sampleData from '../../../../server/database/data/sampleFeed.json';
 
 // import ProfileCard from './ProfileCard';
 // import FilterButtons from './FilterButtons';
@@ -12,20 +12,69 @@ class MainPage extends React.Component {
     super(props);
 
     this.state = {
-      data: data,
+      data: sampleData,
+      displayedData: sampleData,
     };
+
+    this.handleFilter = this.handleFilter.bind(this);
   }
 
-  render() {
+  handleFilter(filter) {
     const { user } = this.props;
     const { data } = this.state;
 
+    if (filter === 'home') {
+      this.setState({
+        displayedData: data,
+      });
+    } else if (filter === 'giveHelp') {
+      let feed = [];
+
+      for (let i = 0; i < data.length; i += 1) {
+        feed = feed.concat(data[i].projects);
+      }
+
+      this.setState({
+        displayedData: feed,
+      });
+    } else if (filter === 'getHelp') {
+      let feed = [];
+
+      for (let i = 0; i < data.length; i += 1) {
+        feed = feed.concat(data[i].tools);
+      }
+
+      this.setState({
+        displayedData: feed,
+      });
+    } else if (filter === 'favorites') {
+      const feed = [];
+
+      for (let i = 0; i < data.length; i += 1) {
+        for (let j = 0; j < data[i].projects.length; j += 1) {
+          const project = data[i].projects[j];
+          if (user.favorites[project._id]) {
+            feed.push(data[i].projects[j]);
+          }
+        }
+      }
+
+      this.setState({
+        displayedData: feed,
+      });
+    }
+  }
+
+  render() {
+    // const { user } = this.props;
+    const { displayedData } = this.state;
+
     return (
       <div>
-        <ProfileCard user={user} />
+        {/* <ProfileCard user={user} />
         <FilterButtons />
-        <Messages />
-        <FeedContainer data={data} />
+        <Messages /> */}
+        <FeedContainer data={displayedData} />
       </div>
     );
   }
