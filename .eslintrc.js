35b1f99d50095abf14bd6116da8773265fa79f6a
Mikl Barton <miklbarton@gmail.com>
2021-04-06 16:08:17 -0600

    Fixed linter errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,6 +19,6 @@ module.exports = {
     'react',
   ],
   rules: {
-    allowFunctionParams: true,
+    allowFunctionParams: 0,
   },
 };
--- client/src/components/MessageButton.jsx
+++ client/src/components/MessageButton.jsx
@@ -1,6 +1,5 @@
-
 import React, { Component } from 'react';
-import Talk from "talkjs";
+import Talk from 'talkjs';
 import DummyUser from '../../../server/database/data/sampleUser.json';
 import DummyFeed from '../../../server/database/data/sampleFeed.json';
 
@@ -14,41 +13,41 @@ class MessageButton extends React.Component {
     const { user = DummyUser, otherUser = DummyFeed[3] } = this.props;
     Talk.ready
       .then(() => {
-          user.id = user._id;
-          user.role = 'member';
-          const me = new Talk.User(user);
-          otherUser.id = otherUser._id;
-          otherUser.role = 'member';
-          const other = new Talk.User(otherUser);
+        user.id = user._id;
+        user.role = 'member';
+        const me = new Talk.User(user);
+        otherUser.id = otherUser._id;
+        otherUser.role = 'member';
+        const other = new Talk.User(otherUser);
 
-          if (!window.talkSession) {
-              window.talkSession = new Talk.Session({
-                  appId: "tsdPQIUx",
-                  me,
-              });
-          }
+        if (!window.talkSession) {
+          window.talkSession = new Talk.Session({
+            appId: 'tsdPQIUx',
+            me,
+          });
+        }
 
-          const conversationId = Talk.oneOnOneId(me, other);
-          const conversation = window.talkSession.getOrCreateConversation(conversationId);
+        const conversationId = Talk.oneOnOneId(me, other);
+        const conversation = window.talkSession.getOrCreateConversation(conversationId);
 
-          conversation.setParticipant(me);
-          conversation.setParticipant(other);
+        conversation.setParticipant(me);
+        conversation.setParticipant(other);
 
-          this.chatbox = window.talkSession.createChatbox(conversation);
-          this.chatbox.mount(this.container);
+        this.chatbox = window.talkSession.createChatbox(conversation);
+        this.chatbox.mount(this.container);
       })
-      .catch(e => console.error(e));
+      .catch((e) => console.error(e));
   }
 
   render() {
     return (
-    <>
-      <button onClick={this.handleClick}>Message</button>
+      <>
+        <button onClick={this.handleClick}>Message</button>
 
-      <div className="chatbox-container" ref={c => this.container = c} />
-    </>
-    )
+        <div className="chatbox-container" ref={(c) => this.container = c} />
+      </>
+    );
   }
-};
+}
 
 export default MessageButton;
