2b978f5122f1612246258d855abc9fa64ff05dac
Krista <kb9286@gmail.com>
2021-04-05 19:17:14 -0500

    adding route and query for adding needed tools

---
+++ server/routes/projects/putNeeds.js
@@ -0,0 +1,13 @@
+const { addNeeds } = require('../../database/queries/projects/addNeeds.js');
+
+exports.putNeeds = (req, res) => {
+  const user = req.params.user_id;
+  const project = req.params.project_id;
+  const update = req.body.needed_tools;
+
+  addNeeds(project, user, update, (err, result) => {
+    if (err) { res.status(404).send(err) }
+    else
+    res.status(200).send(result);
+  });
+};
