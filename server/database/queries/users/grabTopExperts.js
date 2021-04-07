396b78b817f2f5c1357071c71fc2fff4b9d4dfb3
Krista <kb9286@gmail.com>
2021-04-07 18:08:25 -0500

    adding limits

--- server/database/queries/users/grabTopExperts.js
+++ server/database/queries/users/grabTopExperts.js
@@ -3,6 +3,7 @@ const db = require('../../index');
 exports.grabTopExperts = (cb) => {
   db.User.find({})
     .sort({ handy: -1 })
+    .limit(5)
     .exec((err, result) => {
       if (err) return cb(err, null);
       return cb(null, result);
