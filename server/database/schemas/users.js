c5b5b9f7e4aa066584e6cb081ec0217613fa849d
Krista <kb9286@gmail.com>
2021-04-09 14:14:15 -0500

    double quoting escape quotes to correctly import project info to mongodb

--- server/database/schemas/users.js
+++ server/database/schemas/users.js
@@ -5,7 +5,7 @@ exports.usersSchema = mongoose.Schema({
   zip: { type: Number, default: 80301 },
   password: String,
   email: String,
-  photo: { type: String, default: 'http://via.placeholder.com/640x360' },
+  photo: { type: String, default: 'http://via.placeholder.com/320x240' },
   handy: { type: Number, default: 0 },
   report: { type: Number, default: 0 },
   tools: [{
