095265204168c7bcd3cd869ca890ba517f044f0a
Krista <kb9286@gmail.com>
2021-04-09 09:18:20 -0500

    adding seed outline

--- server/database/data/seed.js
+++ server/database/data/seed.js
@@ -1,8 +1,3 @@
-//import users
-//get all user_ids
-//insert tools @ random id (assign id randomly to json file? then import)
-//insert projects @ random id
-
-//or import all
+//import all
 //get user ids, get tool ids, get project ids
-//assign refs randomly
+//assign refs
