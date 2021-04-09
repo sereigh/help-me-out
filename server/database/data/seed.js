d74dd1697f5b3d59ac54c48cbfcaeff0f5ccf5ab
Krista <kb9286@gmail.com>
2021-04-09 10:35:03 -0500

    adding interim seed commands

--- server/database/data/seed.js
+++ server/database/data/seed.js
@@ -1,3 +1,12 @@
-//import all
-//get user ids, get tool ids, get project ids
-//assign refs
+/*
+1) start mongod
+2) open terminal
+3) enter import commands via command line one at a time
+
+  mongoimport --db=help-me-out --headerline --type=csv --ignoreBlanks --file ./users.csv
+
+  mongoimport --db=help-me-out --headerline --type=csv --ignoreBlanks --file=./server/database/data/mockups/mocks/mockProjects.json
+
+  mongoimport --db=help-me-out --headerline --type=csv --ignoreBlanks --file=./server/database/data/mockups/mocks/mockTools.json
+
+*/
