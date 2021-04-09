7fc31038bad84341ef53fc5608cd6a280621e875
Krista <kb9286@gmail.com>
2021-04-09 12:39:41 -0500

    escaping commas in projects.csv

--- server/database/data/seed.js
+++ server/database/data/seed.js
@@ -3,10 +3,9 @@
 2) open terminal
 3) enter import commands via command line one at a time
 
-  mongoimport --db=help-me-out --headerline --type=csv --ignoreBlanks --file ./users.csv
+  mongoimport --db=help-me-out --headerline --type=csv --ignoreBlanks --file ./server/database/data/mockups/mocks/users.csv
 
-  mongoimport --db=help-me-out --headerline --type=csv --ignoreBlanks --file=./server/database/data/mockups/mocks/mockProjects.json
+  mongoimport --db=help-me-out --headerline --type=csv --ignoreBlanks --file=./server/database/data/mockups/mocks/projects.csv
 
-  mongoimport --db=help-me-out --headerline --type=csv --ignoreBlanks --file=./server/database/data/mockups/mocks/mockTools.json
-
+  mongoimport --db=help-me-out --headerline --type=csv --ignoreBlanks --file=./server/database/data/mockups/mocks/tools.csv
 */
