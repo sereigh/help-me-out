3641affeca45f1b36f81649a4fa181558477a46a
tillmattson <tillmattson@gmail.com>
2021-04-10 11:57:21 -0600

    styling

--- client/src/components/LandingPage/LandingPage.jsx
+++ client/src/components/LandingPage/LandingPage.jsx
@@ -21,13 +21,13 @@ class LandingPage extends React.Component {
     const { projects, experts, loginText } = this.state;
     return (
       <div className="landing-page">
-        <div>{loginText}</div>
-        <h2>Community Projects</h2>
+        <h1 className="login-text">{loginText}</h1>
+        <h2 className="lpprojectshead">Community Projects</h2>
         <div className="lpProjectsContainer">
           {/* <h4 className="lpFeedHeaders">Check out these recent projects</h4> */}
           <LPProjects projects={projects} onClick={this.onClick} />
         </div>
-        <h2>Community Experts</h2>
+        <h2 className="lpexpertshead">Community Experts</h2>
         <div className="lpExpertsContainer">
           {/* <h4 className="lpFeedHeaders">Experts in your area</h4> */}
           <LPExperts experts={experts} onClick={this.onClick} />
